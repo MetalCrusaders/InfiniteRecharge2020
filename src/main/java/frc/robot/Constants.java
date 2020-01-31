388843fc18205396897831adb29116d174940b38
MetalCrusaders <jooliasemail@gmail.com>
2020-01-31 17:00:04 -0500

    Inital

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    // Motors
+    public static final int DRIVETRAIN_LEFT_TALON = 0;
+    public static final int DRIVETRAIN_RIGHT_TALON = 1;
+    public static final int WHEEL_MOTOR = 2;
+    public static final int SHOOTER_LEFT_VICTOR = 3;
+    public static final int SHOOTER_RIGHT_VICTOR = 4;
+    public static final int INTAKE_TALON = 5;
+
+    // Controllers
+    public static final int kController1 = 0;
+    public static final int kController2 = 1;
+
+}
